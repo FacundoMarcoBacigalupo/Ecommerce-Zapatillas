041357c62b4b6622a86fe3e070bd1a2e987a79c8
Facundo Marco Bacigalupo <marcofacundolucas@gmail.com>
2023-05-15 17:07:48 -0300

    Corección Json

--- src/Componentes/Historia/Historia.jsx
+++ src/Componentes/Historia/Historia.jsx
@@ -10,7 +10,11 @@ const Historia = () => {
         <main id='historiaNbstyle'>
             <h1>Historia</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati pariatur dolores similique deserunt asperiores expedita laboriosam voluptatum, fugiat, minus deleniti nobis dignissimos inventore! Nam qui at animi ipsum magnam ratione!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In sequi eveniet ex soluta quidem eligendi consequuntur, itaque repellendus qui libero ipsum, delectus, similique error. Nesciunt recusandae aperiam consectetur et exercitationem!
+            Pariatur id sit mollitia, blanditiis iusto ab nemo ex. Veritatis ut dolor culpa eaque voluptatibus dignissimos temporibus at dicta magnam, maxime facere blanditiis numquam repellat eos officia alias ex fuga!
+            Vero asperiores quas odio qui obcaecati consectetur, error distinctio voluptatibus harum beatae ut quasi minus cupiditate nihil, pariatur cumque eaque illum est eum, iste maiores? Ipsum accusamus mollitia atque similique!
+            Similique in ducimus temporibus laudantium voluptatem fuga eligendi tempora quia sequi animi, nemo optio molestiae. Ullam necessitatibus dolorum quam corrupti eos asperiores tempore, nulla repellat id cum consequatur, sapiente recusandae.
+            Saepe iste provident praesentium? Magnam odit quisquam numquam ullam placeat minus architecto quis sed ipsam fuga libero quod aut ipsum, sequi dolores, tenetur accusamus sit corporis nihil aspernatur consequuntur. Aut?</p>
         </main>
     )
 }
